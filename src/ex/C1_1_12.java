a98bb0ccc4c4cf189f5ca52bb5501337eb0ad360
王海鹏 <whp98@qq.com>
2018-08-06 20:25:55 +0800

    没有啥意义的代码，只是跑跑看看结果

--- src/ex/C1_1_12.java
+++ src/ex/C1_1_12.java
@@ -1,13 +1,13 @@
 package ex;
-
+//没有啥意义的代码，只是跑跑看看结果
 public class C1_1_12 {
 	public static void main(String[] args) {
-		int [] a = new int[10];
-		for(int i = 0; i<10; i++)
+		int[] a = new int[10];
+		for (int i = 0; i < 10; i++)
 			a[i] = 9 - i;
-		for(int i = 0; i < 10; i++)
+		for (int i = 0; i < 10; i++)
 			a[i] = a[a[i]];
-		for(int i = 0; i < 10; i++)
+		for (int i = 0; i < 10; i++)
 			System.out.println(i);
 		System.out.println(a);
 	}
