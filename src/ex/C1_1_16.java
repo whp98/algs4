7cda507be4092e15846ae5bb7111e1372fb67a06
王海鹏 <whp98@qq.com>
2018-08-07 12:25:52 +0800

    简单的字符处理递归算法

--- src/ex/C1_1_16.java
+++ src/ex/C1_1_16.java
@@ -1,11 +1,14 @@
 package ex;
 
+//一种用于处理数字的递归算法
 public class C1_1_16 {
 	public static void main(String[] args) {
 		System.out.println(exR1(45));
 	}
+
 	public static String exR1(int n) {
-		if(n <= 0) return "";
+		if (n <= 0)
+			return "";
 		return exR1(n - 3) + n + exR1(n - 2) + n;
 	}
 }
