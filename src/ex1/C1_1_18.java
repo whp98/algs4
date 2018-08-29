d134a584cd23e748a41efd0f3d64c96cd4b5671a
王海鹏 <whp98@qq.com>
2018-08-29 14:45:36 +0800

    计算乘法的神秘算法，和更改后几乎没有规律的算法

--- src/ex1/C1_1_18.java
+++ src/ex1/C1_1_18.java
@@ -2,23 +2,36 @@ package ex1;
 
 public class C1_1_18 {
 	public static void main(String[] args) {
-		System.out.println(mystery(2,25));
+		long start=System.currentTimeMillis(); //获取开始时间
+		System.out.println(3/2);
+		System.out.println(mystery(2, 5));
 		System.out.println(mystery(3, 11));
-		System.out.println(new_mystery(2, 25));
-		System.out.println(new_mystery(3, 11));
+		System.out.println(mystery(2, 3));
+//		System.out.println(new_mystery(2, 3));
+//		System.out.println(new_mystery(2, 4));
+		for(int i = 0; i <= 100 ;i++) {
+			System.out.println(new_mystery(2, i));
+		}
+		long end=System.currentTimeMillis(); //获取结束时间
+		System.out.println("程序运行时间： "+(end-start)+"ms");  
+		
 	}
-	
-	//可以从结果看出mystery可以用来算乘法返回值为a*b
+
+	// 可以从结果看出mystery可以用来算乘法返回值为a*b
 	public static int mystery(int a, int b) {
-		if (b==0) return 0;
-		if (b%2 ==0) return mystery(a+a, b/2);
-		return mystery(a+a, b/2)+a;
+		if (b == 0)
+			return 0;
+		if (b % 2 == 0)
+			return mystery(a + a, b / 2);
+		return mystery(a + a, b / 2) + a;
 	}
-	
-	//
+
+	//更改代码后明显不知道计算的是什么
 	public static int new_mystery(int a, int b) {
-		if (b==0) return 1;
-		if (b%2 ==0) return mystery(a*a, b/2);
-		return mystery(a*a, b/2)*a;
+		if (b == 0)
+			return 1;
+		if (b % 2 == 0)
+			return mystery(a * a, b / 2);
+		return mystery(a * a, b / 2) * a;
 	}
 }
